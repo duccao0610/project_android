86f1ef908d99cc0b7f656ae3ca9c52ca2471426e
duccao0610 <caominhduc610@gmail.com>
2019-07-10 23:07:18 +0700

    gg sign in

--- app/src/main/java/com/example/dogs/SignInActivity.kt
+++ app/src/main/java/com/example/dogs/SignInActivity.kt
@@ -7,19 +7,10 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.google.android.gms.auth.api.Auth
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.SignInButton
-import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.GoogleApiClient
-import com.google.android.gms.common.api.ResultCallback
-import com.google.android.gms.common.api.Status
-import com.google.android.gms.tasks.Task
-import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.activity_sign_in.*
-const val RC_SIGN_IN =123
 class SignInActivity : AppCompatActivity(),GoogleApiClient.OnConnectionFailedListener {
     override fun onConnectionFailed(connectionResult: ConnectionResult) {
         Log.d("duc","onConnectionFailed" + connectionResult)
