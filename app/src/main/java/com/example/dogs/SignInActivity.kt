09d91a4704bf6ebc4a941c2d54ee2f3f83206a1a
duccao0610 <caominhduc610@gmail.com>
2019-07-10 23:03:20 +0700

    add file

--- app/src/main/java/com/example/dogs/SignInActivity.kt
+++ app/src/main/java/com/example/dogs/SignInActivity.kt
@@ -1,12 +1,96 @@
 package com.example.dogs
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.api.ResultCallback
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_sign_in.*
+const val RC_SIGN_IN =123
+class SignInActivity : AppCompatActivity(),GoogleApiClient.OnConnectionFailedListener {
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        Log.d("duc","onConnectionFailed" + connectionResult)
+    }
 
-class SignInActivity : AppCompatActivity() {
+    private val RC_SIGN_IN = 9001
+    private var mGoogleApiClient : GoogleApiClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+
+        btn_signin.setOnClickListener {
+            emailValidation()
+        }
+
+        updateUI(false)
+
+        val gso  = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+            .enableAutoManage(this,this)
+            .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+            .build()
+
+        btn_gg.setOnClickListener(View.OnClickListener {
+            var signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
+            startActivityForResult(signInIntent,RC_SIGN_IN)
+        })
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_SIGN_IN){
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            updateUI(result.isSuccess)
+        }
+    }
+
+    private fun updateUI(isLogIn: Boolean) {
+        if(isLogIn){
+            Toast.makeText(applicationContext,"Logged In", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        }
+        else{
+            btn_gg.visibility = View.VISIBLE
+        }
+    }
+
+    fun emailValidation(){
+        var email : String = edit1.getText().toString().trim()
+        if (android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            passValidation()
+        }
+        else{
+            Toast.makeText(applicationContext,"Invalid email address", Toast.LENGTH_SHORT).show()
+        }
+    }
+    fun passValidation(){
+        var pass : String  = edit2.getText().toString().trim()
+        if(pass.length <=6){
+            Toast.makeText(applicationContext,"Password format not match", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(applicationContext,"Logged In", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
